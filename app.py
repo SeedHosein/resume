66a1f141443517927f1baec6ec28fd6140526cdc
SeedHosein <SeedHosein0@gmail.com>
2025-10-10 02:23:50 +0330

    add sitemap.xml and robots.txt

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, Response, request, redirect, url_for, render_template
+from flask_sitemapper import Sitemapper
 
 data = {
     "firstname": "سیدحسین",
@@ -117,19 +118,43 @@ data = {
     }
 }
 
+sitemapper = Sitemapper()
 
 app = Flask(
         __name__,
         static_folder='static',
         )
+sitemapper.init_app(app)
+
+
+@sitemapper.include(changefreq="weekly", priority=1.0)
 @app.route("/fa")
 @app.route("/")
 def index():
     return redirect(url_for('resume_fa'))
 
+@sitemapper.include(changefreq="daily", priority=0.9)
 @app.route("/fa/resume")
 def resume_fa():
     return render_template('resume.html', **data)
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+  return sitemapper.generate()
+
+@app.route('/robots.txt')
+def noindex():
+    robots_txt = f'''
+User-agent: *
+Allow: /
+Disallow: /404/
+
+Sitemap: { url_for('sitemap', _external=True) }
+    '''
+    r = Response(response=robots_txt, status=200, mimetype="text/plain")
+    r.headers["Content-Type"] = "text/plain; charset=utf-8"
+    return r
 
 
 if __name__ == '__main__':
