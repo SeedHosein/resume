16bd0317cd8354130705c9448a0b9f0f4f4b1a7c
SeedHosein <SeedHosein0@gmail.com>
2025-11-10 02:32:23 +0330

    add english language page

--- app.py
+++ app.py
@@ -14,18 +14,33 @@ sitemapper.init_app(app)
 
 
 @sitemapper.include(changefreq="weekly", priority=1.0)
-@app.route("/fa")
 @app.route("/")
 def index():
+    return redirect(url_for('resume_en'))
+
+@app.route("/en/")
+def redirect_en():
+    return redirect(url_for('resume_en'))
+
+@app.route("/fa/")
+def redirect_fa():
     return redirect(url_for('resume_fa'))
 
 @sitemapper.include(changefreq="daily", priority=0.9)
-@app.route("/fa/resume")
+@app.route("/fa/resume/")
 def resume_fa():
-    with open("./data.json", "r") as f:
+    with open("./data-fa.json", "r") as f:
         data = json.load(f)
 
-    return render_template('resume.html', **data)
+    return render_template('resume-fa.html', url_external=url_for('resume_fa', _external=True), **data)
+
+@sitemapper.include(changefreq="daily", priority=0.9)
+@app.route("/en/resume/")
+def resume_en():
+    with open("./data-en.json", "r") as f:
+        data = json.load(f)
+
+    return render_template('resume-en.html', url_external=url_for('resume_en', _external=True), **data)
 
 
 @app.route("/sitemap.xml")
